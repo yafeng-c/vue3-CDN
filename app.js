708b8887aaa5abe81976aba580c44ae4ca2cc082
Y Chen <yafeng_chen@outlook.com>
2020-12-09 23:44:21 +0100

    learing

---
+++ app.js
@@ -0,0 +1,32 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      showBooks: true,
+      title: "The Final Empire",
+      author: "Brandon",
+      age: 45,
+      x: 0,
+      y: 0,
+    };
+  },
+  methods: {
+    changeTitle(title) {
+      this.title = title;
+    },
+    toggleShowBooks() {
+      this.showBooks = !this.showBooks;
+    },
+    handleEvent(e, data) {
+      console.log(e, e.type);
+      if (data) {
+        console.log(data);
+      }
+    },
+    handleMousemove(e) {
+      this.x = e.offsetX;
+      this.y = e.offsetY;
+    },
+  },
+});
+
+app.mount("#app");

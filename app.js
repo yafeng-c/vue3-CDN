483dce034e5d5ef6bcbc4fc92ce487dbe9c74eee
Y Chen <yafeng_chen@outlook.com>
2021-03-06 15:34:50 +0100

    test

--- app.js
+++ app.js
@@ -2,8 +2,29 @@ const app = Vue.createApp({
   data() {
     return {
       showBooks: true,
+      books: [
+        {
+          title: "name of the wind",
+          author: "patrick rothfuss",
+          img: "assets/1.jpg",
+          isFav: true,
+        },
+        {
+          title: "the way of kings",
+          author: "brandon sanderson",
+          img: "assets/2.jpg",
+          isFav: false,
+        },
+        {
+          title: "the final empire",
+          author: "brandon sanderson",
+          img: "assets/3.jpg",
+          isFav: true,
+        },
+      ],
       title: "The Final Empire",
       author: "Brandon",
+      url: "http://www.google.com",
       age: 45,
       x: 0,
       y: 0,
@@ -25,6 +46,14 @@ const app = Vue.createApp({
     handleMousemove(e) {
       this.x = e.offsetX;
       this.y = e.offsetY;
+    },
+    toggleFav(book) {
+      book.isFav = !book.isFav;
+    },
+  },
+  computed: {
+    filterBooks() {
+      return this.books.filter((book) => book.isFav);
     },
   },
 });
